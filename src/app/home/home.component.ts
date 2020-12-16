60a98caff1baca1c7d5c7b9b75c3bd9005d363c8
Abhishek <developerweb6@gmail.com>
2020-12-16 12:42:22 +0530

    by harkamal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -13,3 +14,4 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
