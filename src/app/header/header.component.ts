1320335e74bb7ecba7d431931a2a9271da4998c8
vijay <vsingh@codenomad.com>
2020-12-23 11:57:37 +0530

    vijay

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup} from '@angular/forms';
+import { LoginService } from '../services/login.service';
+import {HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  login_form : boolean  = false;
+  success_message : string = "Login Successfully";
+  error_message : string = "Login Failed";
+  success_div : boolean = false;
+  error_div : boolean = false;
+  signin = new FormGroup({
+    signin: new FormGroup({
+       //name_person: new FormControl(''),
+       email: new FormControl(''),
+       password: new FormControl(''),
+       user_type: new FormControl(''),
+       checked: new FormControl(''),
+      // tiktok: new FormControl(''),
+     //  insta: new FormControl(''),
+     //  fb: new FormControl(''),
+     //  twitter: new FormControl('')
+     })
+   });
+
+  constructor(private loginservice:LoginService,private http : HttpClient,private router: Router,) { }
 
   ngOnInit(): void {
+  }
+
+  openLoginForm(){ this.login_form = true; }
+  closeLoginForm(){ this.login_form = false; }
+
+  onSubmit() {
+
+    console.log("vijay here");
+    console.log(this.signin.value);
+    
+    // TODO: Use EventEmitter with form value
+      // this.http.post(this.loginservice.apiUrl,data)
+      
+      // this.router.navigate(['/']);
+      // return false;
+    console.log(this.signin.value);
+    const data = this.signin.value;
+    this.loginservice.login(data)
+    .subscribe(
+      (response) => {                           
+        console.log(response)
+  
+      },
+      (error) => {                            
+        console.error(error)
+      }
+    );
   }
 
 }
